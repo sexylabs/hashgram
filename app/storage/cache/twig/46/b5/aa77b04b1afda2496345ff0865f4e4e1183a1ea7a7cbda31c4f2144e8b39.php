ebb6a0d3b9d8be8059fc2c38c03343c3a1b61851
Victor Martinez <victormartinez@victormartinez.local>
2014-06-04 15:25:46 +0100

    Update bootstrap version

--- app/storage/cache/twig/46/b5/aa77b04b1afda2496345ff0865f4e4e1183a1ea7a7cbda31c4f2144e8b39.php
+++ app/storage/cache/twig/46/b5/aa77b04b1afda2496345ff0865f4e4e1183a1ea7a7cbda31c4f2144e8b39.php
@@ -24,6 +24,12 @@ class __TwigTemplate_46b5aa77b04b1afda2496345ff0865f4e4e1183a1ea7a7cbda31c4f2144
     {
         // line 2
         echo "
+    <script src=\"//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js\"></script>
+    <script type=\"application/javascript\" src=\"";
+        // line 4
+        echo twig_escape_filter($this->env, (isset($context["assetUri"]) ? $context["assetUri"] : null), "html", null, true);
+        echo "/libs/bootstrap/js/bootstrap.min.js\"></script>
+
 ";
     }
 
@@ -34,6 +40,6 @@ class __TwigTemplate_46b5aa77b04b1afda2496345ff0865f4e4e1183a1ea7a7cbda31c4f2144
 
     public function getDebugInfo()
     {
-        return array (  63 => 15,  57 => 14,  49 => 8,  37 => 6,  33 => 4,  29 => 3,  26 => 2,  20 => 1,  252 => 127,  249 => 126,  206 => 71,  203 => 70,  195 => 62,  192 => 61,  188 => 52,  182 => 50,  180 => 49,  172 => 45,  169 => 44,  153 => 15,  150 => 14,  143 => 129,  140 => 126,  124 => 111,  121 => 70,  118 => 69,  115 => 68,  113 => 67,  109 => 65,  106 => 61,  97 => 53,  95 => 44,  75 => 17,  73 => 14,  70 => 13,  61 => 6,  21 => 60,  14 => 12,  107 => 50,  98 => 47,  92 => 46,  88 => 45,  85 => 44,  81 => 43,  65 => 30,  54 => 1,  44 => 15,  31 => 5,  28 => 125,);
+        return array (  30 => 4,  26 => 2,  20 => 1,);
     }
 }
--- app/storage/cache/twig/47/b4/947ddb86483115dea8e401f8e238c82877fa490a486733ab168143e96cd7.php
+++ app/storage/cache/twig/47/b4/947ddb86483115dea8e401f8e238c82877fa490a486733ab168143e96cd7.php
@@ -27,11 +27,11 @@ class __TwigTemplate_47b4947ddb86483115dea8e401f8e238c82877fa490a486733ab168143e
 \t<link rel=\"stylesheet\" href=\"";
         // line 3
         echo twig_escape_filter($this->env, (isset($context["assetUri"]) ? $context["assetUri"] : null), "html", null, true);
-        echo "/css/bootstrap.min.css\">
-        <link rel=\"stylesheet\" href=\"";
+        echo "/libs/bootstrap/css/bootstrap.min.css\">
+    <link rel=\"stylesheet\" href=\"";
         // line 4
         echo twig_escape_filter($this->env, (isset($context["assetUri"]) ? $context["assetUri"] : null), "html", null, true);
-        echo "/css/bootstrap-responsive.min.css\">
+        echo "/libs/bootstrap/css/bootstrap-theme.min.css\">
 
 ";
     }
@@ -43,6 +43,6 @@ class __TwigTemplate_47b4947ddb86483115dea8e401f8e238c82877fa490a486733ab168143e
 
     public function getDebugInfo()
     {
-        return array (  33 => 4,  29 => 3,  26 => 2,  20 => 1,  252 => 127,  249 => 126,  206 => 71,  203 => 70,  195 => 62,  192 => 61,  188 => 52,  182 => 50,  180 => 49,  172 => 45,  169 => 44,  153 => 15,  150 => 14,  143 => 129,  140 => 126,  124 => 111,  121 => 70,  118 => 69,  115 => 68,  113 => 67,  109 => 65,  106 => 61,  97 => 53,  95 => 44,  75 => 26,  73 => 14,  70 => 13,  61 => 6,  21 => 60,  14 => 12,  107 => 50,  98 => 47,  92 => 46,  88 => 45,  85 => 44,  81 => 43,  65 => 30,  54 => 1,  44 => 15,  31 => 4,  28 => 125,);
+        return array (  33 => 4,  29 => 3,  26 => 2,  20 => 1,);
     }
 }
