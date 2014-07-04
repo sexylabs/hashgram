7e546b211913b10400b4dbf0604867a68dcd7737
Victor Martinez <martinezvcr@gmail.com>
2014-07-04 15:24:15 +0100

    Add comment to InstagramService methods

--- app/services/instagram/InstagramService.php
+++ app/services/instagram/InstagramService.php
@@ -11,15 +11,44 @@ namespace App\Services\Instagram;
 
 class InstagramService {
 
+    /**
+     * @cons string
+     */
     const CLIENT_ID 	 = "d2c57fc594d84ccdb4e455beb5da6bd1";
+
+    /**
+     * @var string
+     */
     const CLIENT_SECRET  = "cbe1ee8d4c664f12b548f1a3c790024b";
+
+    /**
+     * @var string
+     */
     const CALLBACK       = "http://localhost:8888/instagram/instagram.class.php";
+
+    /**
+     * @var string
+     */
     const GRANT_TYPE     = "authorization_code";
 
+    /**
+     * @var string
+     */
     const API_URL_BASE   = "https://api.instagram.com/v1/";
+
+    /**
+     * @var string
+     */
     const OAUTH_URL_BASE = "https://api.instagram.com/oauth/";
 
+    /**
+     * @var string
+     */
     private $code;
+
+    /**
+     * @var string
+     */
     private $accessToken;
 
     public function __construct()
@@ -87,21 +116,41 @@ class InstagramService {
         return json_decode($result);
     }
 
+    /**
+     * Return the URL required to access Instagram API
+     *
+     * @return string
+     */
     public function getAuthorizationCodeUrl()
     {
         return self::AUTH_URL_BASE . "authorize/?client_id=" . self::CLIENT_ID . "&redirect_uri=" . self::CALLBACK . "&response_type=code";
     }
 
+    /**
+     * Set access token
+     *
+     * @param $token
+     */
     public function setAccessToken($token)
     {
         $this->accessToken = $token;
     }
 
+    /**
+     * Return the URL required to obtain the token
+     *
+     * @return string
+     */
     public function getAccessTokenUrl()
     {
         return self::OAUTH_URL_BASE . "access_token";
     }
 
+    /**
+     * Set code
+     *
+     * @param string
+     */
     public function setCode($code)
     {
         $this->code = $code;
