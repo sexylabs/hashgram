defbdf72d85fd9f95ba63fd48a400b2429693fca
Victor Martinez <martinezvcr@gmail.com>
2014-07-16 15:32:18 +0100

    Add HashtagController which allows searching for photos based on a tag

---
+++ app/controllers/HashtagController.php
@@ -0,0 +1,58 @@
+<?php
+namespace App\Controllers;
+
+use App\Framework\Mvc\Controller\BasicController;
+use App\Services\Instagram\InstagramService;
+
+
+class HashtagController extends BasicController {
+
+    /**
+     * Show photos based on a hashtag on hashtag.html.twig template
+     *
+     * @param $params
+     */
+    public function listAction($params)
+    {
+        $hashtag = $params["params"][0];
+
+        if ($hashtag)
+        {
+            $this->app->container->singleton('InstagramService', function () {
+                return new InstagramService();
+            });
+
+            try
+            {
+                $instagram = $this->app->InstagramService;
+
+                try
+                {
+                    $result = $instagram->getPhotosByTag($hashtag);
+
+                    $options['hashtag'] = $hashtag;
+                    $options['data']    = $result->data;
+
+                    $this->app->view()->appendData($options);
+                    $this->app->render('templates/home/hashtag.html.twig');
+                }
+                catch (\Exception $e)
+                {
+                    echo $e->getMessage();
+                    $log = $this->app->getLog();
+                    $log->warning($e);
+                }
+            }
+            catch (\Exception $e)
+            {
+                echo $e->getMessage();
+                $log = $this->app->getLog();
+                $log->warning($e);
+            }
+        }
+        else
+        {
+            $this->app->redirect('/');
+        }
+    }
+}
