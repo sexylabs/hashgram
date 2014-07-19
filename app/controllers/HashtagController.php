7df74fbe9a43feb6fb077b88e9fe063c2d3fb130
Adriano Fialho <adriano@afialho.com>
2014-07-18 21:50:00 -0300

    melhorias no codigo

--- app/controllers/HashtagController.php
+++ app/controllers/HashtagController.php
@@ -8,50 +8,45 @@ use App\Services\Instagram\InstagramService;
 class HashtagController extends BasicController {
 
     /**
+     * @var \App\Services\Instagram\InstagramService
+     */
+    private $instagramService;
+
+    public function injectDependencies(){
+        $this->app->container->singleton('InstagramService', function () {
+            return new InstagramService();
+        });
+
+        $this->instagramService = $this->app->InstagramService;
+    }
+
+    /**
      * Show photos based on a hashtag on hashtag.html.twig template
      *
      * @param $params
      */
-    public function listAction($params)
-    {
+    public function listAction($params){
+        /**
+         * Uma outra maneira de pegar as variáveis do POST é assim:
+         * $hashtah = $params['_POST']
+         */
         $hashtag = $this->app->request()->post('hashtag');
 
-        if ($hashtag)
-        {
-            $this->app->container->singleton('InstagramService', function () {
-                return new InstagramService();
-            });
+        if ($hashtag){
+            try{
+                $this->injectDependencies();
+                $result = $this->instagramService->getPhotosByTag($hashtag);
 
-            try
-            {
-                $instagram = $this->app->InstagramService;
-
-                try
-                {
-                    $result = $instagram->getPhotosByTag($hashtag);
-
-                    $options['hashtag'] = $hashtag;
-                    $options['result']  = $result->data;
-
-                    $this->app->view()->appendData($options);
-                    $this->app->render('templates/home/hashtag.html.twig');
-                }
-                catch (\Exception $e)
-                {
-                    echo $e->getMessage();
-                    $log = $this->app->getLog();
-                    $log->warning($e);
-                }
-            }
-            catch (\Exception $e)
-            {
+                $this->app->render('templates/home/hashtag.html.twig', array(
+                    'hashtag' => $hashtag,
+                    'photos' => $result->data
+                ));
+            }catch (\Exception $e){
                 echo $e->getMessage();
                 $log = $this->app->getLog();
                 $log->warning($e);
             }
-        }
-        else
-        {
+        }else{
             $this->app->redirect('/');
         }
     }
