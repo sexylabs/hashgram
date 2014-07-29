7be20ab9f96c81f37a49add0cc3d647e6a753a49
Victor Martinez <martinezvcr@gmail.com>
2014-07-29 22:31:06 +0100

    Improve return message of getPhotosByTag

--- app/services/instagram/InstagramService.php
+++ app/services/instagram/InstagramService.php
@@ -53,7 +53,7 @@ class InstagramService {
                 else
                 {
                     $result["success"]  = FALSE;
-                    $result["message"]  = "Unfortunately we couldn't find any photo with the hashtag <b>#{$tag}</b>.";
+                    $result["message"]  = "Unfortunately we couldn't find any photo with the hashtag '{$tag}'.";
                 }
 
                 return $result;
